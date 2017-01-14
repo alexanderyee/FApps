2b79d3f1941168e15bc8996edce57673b864ae93
Alexander Yee <alexanderyee@email.arizona.edu>
2017-01-14 00:06:19 -0700

    initialized project

--- app/src/main/java/hackaz/bkat/fapps/MainActivity.java
+++ app/src/main/java/hackaz/bkat/fapps/MainActivity.java
@@ -1,5 +1,6 @@
 package hackaz.bkat.fapps;
 
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        PackageManager pm = getPackageManager();
+        pm.getInstalledApplications(PackageManager.GET_META_DATA);
     }
 }
